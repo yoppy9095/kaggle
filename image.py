cf1b751bb99b424fa99ac58b2b167eff5ae8d0ff
zexcell <zexcell.fps@gmail.com>
2021-09-20 17:11:49 +0900

    ｋｋｋ

--- image.py
+++ image.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-
 import tensorflow as tf
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras import optimizers
@@ -9,10 +8,11 @@ from tensorflow.keras.utils import plot_model, to_categorical
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
+#csvデータの読み込み
 train = pd.read_csv("titanic/train.csv", index_col=0).replace("male",0).replace("female",1).replace("S",0).replace("C",1).replace("Q",2)
 test = pd.read_csv("titanic/test.csv", index_col=0).replace("male",0).replace("female",1).replace("S",0).replace("C",1).replace("Q",2)
 answer = pd.read_csv("titanic/gender_submission.csv", index_col=0)
-#csvデータの読み込み
+
 
 #print(train.head()) #先頭5データを出力
 #print(train.describe()) 基本統計量
@@ -24,34 +24,38 @@ def loss_table(df):
     columns = {0 : "欠損数", 1 : "%"})
     return loss_table_ren_columns
 
+
+#欠損データの確認
 #print(loss_table(train))
 #print(loss_table(test))
 
-#欠損データの確認
 
+#欠損データの補完
 train["Age"] = train["Age"].fillna(train["Age"].median())
 train["Embarked"] = train["Embarked"].fillna(0)
-#欠損データの補完
 
-#print(loss_table(train))
 #欠損データ補完の確認
+#print(loss_table(train))
+
 
 #print(train.head(10))
 
+#テストデータの補完
 test["Age"] = test["Age"].fillna(test["Age"].median())
 test["Fare"] = test["Fare"].fillna(test["Fare"].median())
-#テストデータの補完
+
 #print(loss_table(test))
 
+#必要データの抽出
 drop_columns = ["Name","Cabin","Fare","Ticket"]
 train_modify = train.drop(drop_columns, axis=1)
 y_train = train_modify["Survived"]
 x_train = train_modify.drop(labels = ["Survived"], axis = 1)
 
 test_modify = test.drop(drop_columns, axis=1)
-#必要データの抽出
 
 
+#モデル構築
 # model = Sequential()
 
 # model.add(Dense(256, input_dim = 6))
@@ -74,9 +78,9 @@ test_modify = test.drop(drop_columns, axis=1)
 # model.compile(optimizer=adam, 
 #               loss='binary_crossentropy', 
 #               metrics=['accuracy'])
-# #モデル構築
 
-# history = model.fit(x_train, y_train, batch_size=128, epochs=500, verbose=1, validation_split=0.1)
+#実行
+history = model.fit(x_train, y_train, batch_size=128, epochs=500, verbose=1, validation_split=0.1)
 
 # model.save("model.h5")
 
@@ -103,6 +107,5 @@ for item in answer["Survived"]:
     if predict_data == item:
         count += 1
     i+=1
-
-print(count/len(answer))
 #テストデータ出力
+print(count/len(answer))
