a01a45e41c10aae8ac9bf9476e55d552c8b5b6ca
zexcell <zexcell.fps@gmail.com>
2021-09-20 14:02:13 +0900

    ggg

--- image.py
+++ image.py
@@ -3,19 +3,15 @@ import pandas as pd
 
 import tensorflow as tf
 from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.layers import MaxPool2D
 from tensorflow.keras import optimizers
-from tensorflow.keras.layers import Dense, Activation, Dropout, Flatten, Input, Lambda, Conv2D, MaxPooling2D, BatchNormalization
+from tensorflow.keras.layers import Dense, Activation, Dropout, Flatten, Input, Lambda, MaxPooling2D, BatchNormalization
 from tensorflow.keras.utils import plot_model, to_categorical
-from tensorflow.keras.datasets import cifar10
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
 train = pd.read_csv("titanic/train.csv", index_col=0).replace("male",0).replace("female",1).replace("S",0).replace("C",1).replace("Q",2)
 test = pd.read_csv("titanic/test.csv", index_col=0).replace("male",0).replace("female",1).replace("S",0).replace("C",1).replace("Q",2)
 #csvデータの読み込み
-
 
 #print(train.head()) #先頭5データを出力
 #print(train.describe()) 基本統計量
@@ -46,5 +42,40 @@ test["Fare"] = test["Fare"].fillna(test["Fare"].median())
 #テストデータの補完
 #print(loss_table(test))
 
+drop_columns = ["Name","Cabin","Fare","Ticket"]
+train_modify = train.drop(drop_columns, axis=1)
+y_train = train_modify["Survived"]
+x_train = train_modify.drop(labels = ["Survived"], axis = 1)
+#必要データの抽出
 
 
+model = Sequential()
+
+model.add(Dense(4, input_dim = 4 ))
+model.add(Activation('relu'))
+
+model.add(Dense(3))
+model.add(Activation('relu'))
+
+model.add(Dense(2))
+model.add(Activation('relu'))
+
+model.add(Dense(1))
+model.add(Activation('softmax'))
+
+adam = optimizers.Adam(lr=1e-4)
+model.compile(optimizer=adam, 
+              loss='binary_crossentropy', 
+              metrics=['accuracy'])
+#モデル構築
+
+history = model.fit(x_train, y_train, batch_size=128, epochs=500, verbose=1, validation_split=0.1)
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.grid()
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.show()
