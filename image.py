e4f2aa5f2984d8276afc4221e39d9b12c7cdaa29
siroy <is0408pv@gmail.com>
2021-09-20 18:19:59 +0900

    aaaaaaaaaa

--- image.py
+++ image.py
@@ -31,60 +31,71 @@ y=pd.read_csv("train.csv",usecols=[1])
 x=train_csv.drop(["PassengerId","Name",'Survived',"Cabin","Age","Ticket"],axis=1)
 x_test=test_csv.drop(["PassengerId","Name","Cabin","Age","Ticket"],axis=1)
 
-x_test=x_test.dropna(how="all")
-x=x.dropna(how='all')
+# x_test=x_test.dropna(how="any")
+# x=x.dropna(how='any')
+
+x_test=x_test.fillna(x_test.mean())
+x=x.fillna(x.mean())
 
 #欠損地確認 
-# print(x.isnull().sum())
+print(x.isnull().sum())
+print(x_test.isnull().sum())
 
 from sklearn.model_selection import train_test_split
 x_train,x_valid,y_train,y_valid = train_test_split(x,y, train_size=0.8, test_size=0.2)
 
-# # # モデル構築
-model = Sequential()
-
-model.add(Dense(256, input_dim = 6,activation="relu"))
-
-model.add(Dense(256, activation='relu'))
-model.add(Dense(1, activation='softmax'))
-
-model.compile(loss='binary_crossentropy',
-            optimizer=RMSprop(),
-            # optimizer=SGD(lr=0.1),  
-            metrics=['accuracy'])
 
 
-history = model.fit(x_train, y_train,batch_size=256,epochs=50,verbose=1,validation_data=(x_valid, y_valid))
+# # # # モデル構築
+# model = Sequential()
+
+# model.add(Dense(256, input_dim = 6,activation="relu"))
+# model.add(Dropout(0.5))
+
+# model.add(Dense(256, activation='relu'))
+# model.add(Dropout(0.5))
+
+# model.add(Dense(1, activation='softmax'))
+
+# model.compile(loss='binary_crossentropy',
+#             # optimizer=RMSprop(),
+#             optimizer=SGD(lr=0.1),  
+#             metrics=['accuracy'])
+
+
+# history = model.fit(x_train, y_train,batch_size=256,epochs=150,verbose=1,validation_data=(x_valid, y_valid))
                         
-model.save("test.h5")
+# model.save("test.h5")
 
-score = model.evaluate(x_valid,y_valid,  verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+# score = model.evaluate(x_valid,y_valid,  verbose=0)
+# print('Test loss:', score[0])
+# print('Test accuracy:', score[1])
 
 new_model = tf.keras.models.load_model("test.h5")
 new_model.summary()
 
-predictions = new_model.predict(x_test)
 
-print(x_test)
+predictions = new_model.predict(x_train)
+
+
+
 count = 0 
 i = 0
 
-# for i in range(418):
-#     print(predictions[i])
+for i in range(418):
+    print(predictions[i])
 
-# for item in y_test["Survived"]:
-#     predict_data = np.argmax(predictions[i])
+for item in y_test["Survived"]:
+    predict_data = np.argmax(predictions[i])
 
-#     # print(predict_data," t ",item)
-#     print(i)
+    # print(predict_data," t ",item)
+    print(i)
 
 
-#     if predict_data == item:
+    if predict_data == item:
 
-#         count += 1
-#     i+=1
+        count += 1
+    i+=1
 
-#テストデータ出力
-# print(count/418)
+テストデータ出力
+print(count/418)
