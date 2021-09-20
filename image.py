f77f4eee8d2557bfbb1a997aac29afa661308240
zexcell <zexcell.fps@gmail.com>
2021-09-20 16:12:22 +0900

    ka

--- image.py
+++ image.py
@@ -11,6 +11,7 @@ from matplotlib import cm
 
 train = pd.read_csv("titanic/train.csv", index_col=0).replace("male",0).replace("female",1).replace("S",0).replace("C",1).replace("Q",2)
 test = pd.read_csv("titanic/test.csv", index_col=0).replace("male",0).replace("female",1).replace("S",0).replace("C",1).replace("Q",2)
+answer = pd.read_csv("titanic/gender_submission.csv", index_col=0)
 #csvデータの読み込み
 
 #print(train.head()) #先頭5データを出力
@@ -46,40 +47,62 @@ drop_columns = ["Name","Cabin","Fare","Ticket"]
 train_modify = train.drop(drop_columns, axis=1)
 y_train = train_modify["Survived"]
 x_train = train_modify.drop(labels = ["Survived"], axis = 1)
+
+test_modify = test.drop(drop_columns, axis=1)
 #必要データの抽出
 
 
-model = Sequential()
+# model = Sequential()
 
-model.add(Dense(256, input_dim = 6))
-model.add(Activation('relu'))
+# model.add(Dense(256, input_dim = 6))
+# model.add(Activation('relu'))
 
-model.add(Dense(256))
-model.add(Activation('relu'))
+# model.add(Dense(256))
+# model.add(Activation('relu'))
 
-model.add(Dense(256))
-model.add(Activation('relu'))
+# model.add(Dense(256))
+# model.add(Activation('relu'))
 
-model.add(Dense(256))
-model.add(Activation('relu'))
+# model.add(Dense(256))
+# model.add(Activation('relu'))
 
 
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+# model.add(Dense(1))
+# model.add(Activation('sigmoid'))
 
-adam = optimizers.Adam(lr=1e-4)
-model.compile(optimizer=adam, 
-              loss='binary_crossentropy', 
-              metrics=['accuracy'])
-#モデル構築
+# adam = optimizers.Adam(lr=1e-4)
+# model.compile(optimizer=adam, 
+#               loss='binary_crossentropy', 
+#               metrics=['accuracy'])
+# #モデル構築
 
-history = model.fit(x_train, y_train, batch_size=128, epochs=500, verbose=1, validation_split=0.1)
+# history = model.fit(x_train, y_train, batch_size=128, epochs=500, verbose=1, validation_split=0.1)
 
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.grid()
-plt.legend(['Train', 'Validation'], loc='upper left')
-plt.show()
+# model.save("model.h5")
+
+# plt.plot(history.history['accuracy'])
+# plt.plot(history.history['val_accuracy'])
+# plt.title('Model accuracy')
+# plt.ylabel('Accuracy')
+# plt.xlabel('Epoch')
+# plt.grid()
+# plt.legend(['Train', 'Validation'], loc='upper left')
+# plt.show()
+
+new_model = tf.keras.models.load_model("model.h5")
+new_model.summary()
+
+predictions = new_model.predict(test_modify)
+
+
+
+count = 0 
+i = 0
+for item in answer["Survived"]:
+    predict_data = np.argmax(predictions[i])
+    if predict_data == item:
+        count += 1
+    i+=1
+
+print(count/len(answer))
+#print(predictions[0])
